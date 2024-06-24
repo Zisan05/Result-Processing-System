759f8cc7bbaa59c8e21774b679b0704f61808a95
Zisan05 <mdzisanislam69@gmail.com>
2024-06-24 19:38:22 +0800

    Sub semester and sub result work done

--- src/Components/Dashboard/DashBoard.jsx
+++ src/Components/Dashboard/DashBoard.jsx
@@ -3,8 +3,16 @@ import { IoIosArrowForward } from "react-icons/io";
 import { PiStudentFill } from "react-icons/pi";
 import { BiSolidBookOpen } from "react-icons/bi";
 import { Outlet } from "react-router-dom";
+import { MdArrowDropDownCircle } from "react-icons/md";
+import { IoMdArrowDropupCircle } from "react-icons/io";
+import { useState } from "react";
 
 const DashBoard = () => {
+
+const [isSemester , setIsSemester] = useState(false);
+const [Result , setResult] = useState(false);
+
+
     return (
         <div>
             <div className="bgpurple py-[10px]">
@@ -17,7 +25,7 @@ const DashBoard = () => {
          {/* dashboard part */}
            <div className="bg-slate-800 text-white w-full md:w-[300px] pb-[50px]">
   
-           <h1 className="text-[20px] pl-[40px] mt-[20px]  ">MAIN CATEGORY</h1>
+           <h1 className="text-[20px] pl-[40px] mt-[20px]  opacity-40">MAIN CATEGORY</h1>
 
 <p className="border-t-2 w-[220px] ml-[20px] mt-[10px]"></p>
 
@@ -27,42 +35,108 @@ const DashBoard = () => {
                 </div>
 
                 <div>
-                    <h1 className="text-[20px] pl-[40px] mt-[20px]  ">APPEARANCE</h1>
+                    <h1 className="text-[20px] pl-[40px] mt-[20px]  opacity-40">APPEARANCE</h1>
 
                     <p className="border-t-2 w-[220px] ml-[20px] mt-[10px]"></p>
 
 
-                    <div className="flex items-center gap-[10px] pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
+                    <div onClick={() => setIsSemester(!isSemester)} className="flex items-center gap-[10px] pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
                     <PiStudentFill  className="text-[35px]"></PiStudentFill >
                     <h1 className="text-[25px]">Semester</h1>
 
-                    <IoIosArrowForward className="text-[25px] ml-[45px]"></IoIosArrowForward>
+                    {
+                        isSemester === false ?  <MdArrowDropDownCircle className="text-[25px] ml-[30px] mt-[5px]"></MdArrowDropDownCircle> :  <IoMdArrowDropupCircle className="text-[25px] ml-[30px] mt-[5px]"></IoMdArrowDropupCircle>
+                    }
+
+                   
                     </div>
+
+{/* semester logic */}
+                    {
+                        isSemester === true ? <div>
+
+                        <div className="flex items-center  pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
+                        <h1 className="text-[25px] ml-[30px]">Semester 1</h1>
+    
+                        <IoIosArrowForward className="text-[25px] ml-[40px]"></IoIosArrowForward>
+                        </div>
+    
+                        <div className="flex items-center  pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
+                        <h1 className="text-[25px] ml-[30px]">Semester 2</h1>
+    
+                        <IoIosArrowForward className="text-[25px] ml-[40px]"></IoIosArrowForward>
+                        </div>
+    
+                        <div className="flex items-center  pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
+                        <h1 className="text-[25px] ml-[30px]">Semester 3</h1>
+    
+                        <IoIosArrowForward className="text-[25px] ml-[40px]"></IoIosArrowForward>
+                        </div>
+    
+                        <div className="flex items-center  pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
+                        <h1 className="text-[25px] ml-[30px]">Semester 4</h1>
+    
+                        <IoIosArrowForward className="text-[25px] ml-[40px]"></IoIosArrowForward>
+                        </div>
+    
+    
+                        <div className="flex items-center  pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
+                        <h1 className="text-[25px] ml-[30px]">Semester 5</h1>
+    
+                        <IoIosArrowForward className="text-[25px] ml-[40px]"></IoIosArrowForward>
+                        </div>
+    
+    
+                        </div>  : ""
+                    }
+
+
 
             
                     <div  className="flex items-center gap-[20px] pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
                     <PiStudentFill  className="text-[35px]"></PiStudentFill >
                     <h1 className="text-[25px]">Exam</h1>
 
-                    <IoIosArrowForward className="text-[25px] ml-[65px]"></IoIosArrowForward>
+                    <IoIosArrowForward className="text-[25px] ml-[55px]"></IoIosArrowForward>
                     </div>
 
 
-                    <div className="flex items-center gap-[20px] pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
+                    <div onClick={() => setResult(!Result)} className="flex items-center gap-[20px] pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
                     <BiSolidBookOpen  className="text-[35px]"></BiSolidBookOpen>
                     <h1 className="text-[25px]">Result</h1>
 
-                    <IoIosArrowForward className="text-[25px] ml-[60px]"></IoIosArrowForward>
+                    {
+                        Result === false ?  <MdArrowDropDownCircle className="text-[25px] ml-[45px] mt-[5px]"></MdArrowDropDownCircle> :  <IoMdArrowDropupCircle className="text-[25px] ml-[45px] mt-[5px]"></IoMdArrowDropupCircle>
+                    }
                     </div>
+
+                    {
+                        Result === true ? <div>
+
+                        <div className="flex items-center  pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
+                        <h1 className="text-[25px] ml-[60px]">GPA</h1>
+    
+                        <IoIosArrowForward className="text-[25px] ml-[80px]"></IoIosArrowForward>
+                        </div>
+    
+                        <div className="flex items-center  pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
+                        <h1 className="text-[25px] ml-[60px]">CGPA</h1>
+    
+                        <IoIosArrowForward className="text-[25px] ml-[65px]"></IoIosArrowForward>
+                        </div>
+
+                        </div> : ""
+                    }
 
                     <div className="flex items-center gap-[10px] pl-[20px] mt-[20px] py-[8px] focus:bg-indigo-600" tabIndex="1">
                     <PiStudentFill  className="text-[35px]"></PiStudentFill >
                     <h1 className="text-[25px]">Calendar</h1>
 
-                    <IoIosArrowForward className="text-[25px] ml-[45px]"></IoIosArrowForward>
+                    <IoIosArrowForward className="text-[25px] ml-[30px]"></IoIosArrowForward>
                     </div>
 
                 </div>
+
             </div>
 
             {/* dashboard side part */}

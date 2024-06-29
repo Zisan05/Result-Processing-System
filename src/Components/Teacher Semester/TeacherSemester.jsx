29062035fb1158e1a7909212fcf6acb767245cb8
Zisan05 <mdzisanislam69@gmail.com>
2024-06-30 04:16:54 +0800

    Teacher semester work done

--- src/Components/Teacher Semester/TeacherSemester.jsx
+++ src/Components/Teacher Semester/TeacherSemester.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { PiExamFill } from "react-icons/pi";
 import { CiMenuKebab } from "react-icons/ci";
+import Swal from "sweetalert2";
 
 const TeacherSemester = () => {
 
@@ -118,6 +119,141 @@ const handleAnswerSheet = (exam_name) => {
     })
 
 }
+
+// Mark giving method
+
+const [examination_id, setExamination_id] = useState('')
+
+const [student_registration , setStudent_registration ] = useState('')
+
+
+const handleMark = e => {
+
+    e.preventDefault();
+        
+  const mark = e.target.mark.value;
+
+  const markData = {examination_id,student_registration,mark};
+
+  console.log(markData);
+
+  fetch(`https://sabihaakterbristy.pythonanywhere.com/user/token/refresh/`,{
+      method:"POST",
+      credentials: "include",
+      headers: {
+          "content-type":"application/json",
+          
+      },
+      body:  JSON.stringify(token) ,
+      
+  })
+  .then(res => res.json())
+  .then(data => {
+  
+ 
+  
+    const newTok =data.access;
+
+
+    fetch('https://sabihaakterbristy.pythonanywhere.com/user/add-result-each-course/',{
+    method:"POST",
+    credentials: "include",
+    headers: {
+        "content-type":"application/json",
+        "Authorization": `Bearer ${newTok}`,
+    },
+    body: JSON.stringify(markData)
+})
+.then(res => res.json())
+.then(data => {
+   
+ console.log(data);
+
+ Swal.fire({
+  title: "Successfull",
+  text: "You successully give the mark",
+  icon: "success",
+  
+});
+  
+e.target.reset();
+
+   
+})
+
+
+
+  })
+
+}
+
+// Create Exam 
+
+const handleCreateExam = e => {
+
+
+  e.preventDefault();
+        
+  const name = e.target.name.value;
+  const course = e.target.course.value;
+  const semester = e.target.semester.value;
+  const deadline = e.target.deadline.value;
+  const question = e.target.question.value;
+
+  const createExamData = {name,course,semester,deadline,question} 
+
+
+  fetch(`https://sabihaakterbristy.pythonanywhere.com/user/token/refresh/`,{
+    method:"POST",
+    credentials: "include",
+    headers: {
+        "content-type":"application/json",
+        
+    },
+    body:  JSON.stringify(token) ,
+    
+})
+.then(res => res.json())
+.then(data => {
+
+
+
+  const newTok =data.access;
+
+
+  fetch('https://sabihaakterbristy.pythonanywhere.com/user/add-examination/',{
+  method:"POST",
+  credentials: "include",
+  headers: {
+      "content-type":"application/json",
+      "Authorization": `Bearer ${newTok}`,
+  },
+  body: JSON.stringify(createExamData)
+})
+.then(res => res.json())
+.then(data => {
+ 
+console.log(data);
+
+Swal.fire({
+title: "Successfull",
+text: "You successully give the mark",
+icon: "success",
+
+});
+
+// e.target.reset();
+
+ 
+})
+
+
+
+})
+
+
+}
+
     
 
 
@@ -138,6 +274,7 @@ const handleAnswerSheet = (exam_name) => {
 
             {/* Buttons */}
 
+     
             <div className="grid  md:grid-cols-4 lg:grid-cols-4 mx-auto md:gap-[5px] lg:gap-[140px]  lg:ml-[120px]">
                 <button onClick={() => handleAnswerSheet("assignment")} className="bgpurple text-[22px] px-[10px] py-[5px] text-white font-semibold rounded-[5px]  mt-[10px] md:mt-0 hover:bg-red-400">Assignment</button>
                 <button onClick={() => handleAnswerSheet("classtest")} className="bgpurple text-[22px] px-[10px] py-[5px] text-white font-semibold rounded-[5px]  mt-[10px] md:mt-0 hover:bg-orange-400">Class-test</button>
@@ -146,22 +283,82 @@ const handleAnswerSheet = (exam_name) => {
 
             </div>
 
+
+            
+
+           
+           
+
             {/* Answer Card */}
 
             {
-                answerData.map(data => ( <div  onClick={() => handleExamDetails(data.id)} className=" mt-[30px] flex items-center gap-[10px] bg-[#ebe4e4a6] md:w-[500px] lg:w-[1000px] lg:ml-[150px] py-[20px] mb-[20px]">
+                answerData.map(data => ( <div  className=" mt-[30px] flex items-center gap-[10px] bg-[#ebe4e4a6] md:w-[500px] lg:w-[1000px] lg:ml-[150px] py-[20px] mb-[20px]">
                 <PiExamFill className="md:text-[30px] lg:text-[50px] ml-[20px]"></PiExamFill>
                
                  <h1 className="text-[14px] md:text-[16px] lg:text-[25px] font-semibold"> Examination : <span className="purple">{data.examination_name}</span>  ;</h1>
                  <h1 className="text-[14px] md:text-[16px] lg:text-[25px] font-semibold">Resgistration no : <span className="purple">{data.student_registration}</span></h1>
  
-                 <CiMenuKebab  onClick={()=>document.getElementById('my_modal_3').showModal()} className=" md:text-[20px] lg:text-[25px] absolute right-[10px] md:right-[0px] lg:right-[250px]"></CiMenuKebab>
+                <div className="flex gap-[10px]">
+                    <a href={data.answer}><button className="bg-indigo-500 text-white mr-[5px]  ml-0 md:ml-0 lg:ml-[40px]  px-[10px] py-[5px] hover:bg-orange-400 font-semibold">View Answer</button></a>
+
+                     
+
+                  <span onClick={() => setStudent_registration(data.student_registration)}><span onClick={() => setExamination_id(data.examination_id)}><button onClick={()=>document.getElementById('my_modal_3').showModal()} className="bg-indigo-500 text-white mr-[5px] ml-0 md:ml-0 lg:ml-[40px]  px-[10px] py-[5px] hover:bg-lime-400 font-semibold">Open</button></span></span>
+
+                </div>
                 
              </div>))
             }
 
+<dialog id="my_modal_3" className="modal">
+  <div className="modal-box">
+    <form method="dialog">
+      {/* if there is a button in form, it will close the modal */}
+      <button  className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    <h3 className="font-bold text-[25px] text-center">Mark Giving proccess</h3>
+    <form  onSubmit={handleMark} className="pl-[30px]"> 
+        <h1 className="text-[20px] mt-[20px] font-semibold">Mark</h1>
+        <input className="bg-slate-200 w-full md:w-[400px] mt-[10px] py-[5px] pl-[10px]" name="mark" placeholder="Give The mark" type="text" />
+
+        <button className="bg-indigo-500 text-white mr-[5px] mt-[10px]  px-[10px] py-[5px] font-semibold rounded-[5px]">Submit</button>
+      </form>
+  </div>
+</dialog>
+
            
-              
+<div className="pb-[20px] mt-[40px]">
+<h1  onClick={()=>document.getElementById('my_modal_4').showModal()} className="text-[55px] relative left-[160px] md:left-[220px] lg:left-[580px] bgpurple w-[75px]   text-white font-semibold pl-[18px] hover:bg-blue-400">+</h1>
+</div>
+
+<dialog id="my_modal_4" className="modal">
+  <div className="modal-box">
+    <form method="dialog">
+      {/* if there is a button in form, it will close the modal */}
+      <button  className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    <h3 className="font-bold text-[25px] text-center">Create New Exam</h3>
+    <form  onSubmit={handleCreateExam} className="pl-[30px]"> 
+
+        <h1 className="text-[20px] mt-[20px] font-semibold">Exam Name</h1>
+        <input className="bg-slate-200 w-full md:w-[400px] mt-[10px] py-[5px] pl-[10px]" name="name" placeholder="Exam name" type="text" />
+
+        <h1 className="text-[20px] mt-[20px] font-semibold">Course</h1>
+        <input className="bg-slate-200 w-full md:w-[400px] mt-[10px] py-[5px] pl-[10px]" name="course" placeholder="course" type="text" />
+
+        <h1 className="text-[20px] mt-[20px] font-semibold">Deadline</h1>
+        <input className="bg-slate-200 w-full md:w-[400px] mt-[10px] py-[5px] pl-[10px]" name="deadline" placeholder="Deadline" type="text" />
+
+        <h1 className="text-[20px] mt-[20px] font-semibold">Semester</h1>
+        <input className="bg-slate-200 w-full md:w-[400px] mt-[10px] py-[5px] pl-[10px]" value={semester_no} name="semester" placeholder="Semester no" type="text" />
+
+        <h1 className="text-[20px] mt-[20px] font-semibold">Question</h1>
+        <input className="bg-slate-200 w-full md:w-[400px] mt-[10px] py-[5px] pl-[10px]" name="question" placeholder="Question PDF URL" type="text" />
+
+        <button className="bg-indigo-500 text-white mr-[5px] mt-[10px]  px-[10px] py-[5px] font-semibold rounded-[5px]">Submit</button>
+      </form>
+  </div>
+</dialog>
 
         </div>
     );
